286e6f5772656967844b14f6ec0ffb69b041cb28
Leticia-Lopes2 <leticiamouralopes5@gmail.com>
2024-02-01 16:07:51 -0300

    feat: Adicionando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 import { AppThemeProvider } from "./shared/contexts/ThemeContext";
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { DarkTheme, LightTheme } from "./shared/themes";
-
 export const App = () => {
 
   return (
